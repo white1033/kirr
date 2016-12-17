dba6d2ede7d63763fc72dc1dfd4d5a7903a9cc85
white1033 <k75715@gmail.com>
2016-12-17 15:10:27 +0800

    add auto shortcode generator

--- src/kirr/shortener/models.py
+++ src/kirr/shortener/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from .utils import create_shortcode
 
 # Create your models here.
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == '':
+            self.shortcode = create_shortcode(self)
+        super(KirrURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
