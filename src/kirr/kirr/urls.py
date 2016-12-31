377d236056f9eb484e1e868b52c71c84bccea2c5
white1033 <k75715@gmail.com>
2016-12-31 16:46:09 +0800

    add a simple form

--- src/kirr/kirr/urls.py
+++ src/kirr/kirr/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shortener.views import HomeView, KirrShortenerView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view()),
+    url(r'^(?P<shortcode>[\w-]+){6,15}/$', KirrShortenerView.as_view()),
 ]
--- src/kirr/shortener/views.py
+++ src/kirr/shortener/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
+from .models import KirrURL
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'shortener/home.html', {})
+
+
+    def post(self, request, *args, **kwargs):
+        return render(request, 'shortener/home.html', {})
+
+class KirrShortenerView(View):
+    def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        return HttpResponseRedirect(obj.url)
